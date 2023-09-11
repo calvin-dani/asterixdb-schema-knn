85056666c2827ee530a2ce196f3191560dd642b0
Calvin Thomas Dani <calvinthomas.dani@gmail.com>
2023-09-11 11:02:13 -0700

    map and item node serialisation for jackson

---
+++ asterixdb/asterix-om/src/main/java/org/apache/asterix/om/lazy/metadata/schema/Serialization/mapSerialization.java
@@ -0,0 +1,45 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.asterix.om.lazy.metadata.schema.Serialization;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.apache.asterix.om.lazy.metadata.schema.AbstractRowSchemaNode;
+import org.apache.asterix.om.types.ATypeTag;
+
+import java.io.IOException;
+import java.util.Map;
+
+public class mapSerialization extends JsonSerializer<Map<ATypeTag, AbstractRowSchemaNode>> {
+
+    @Override
+    public void serialize(Map<ATypeTag, AbstractRowSchemaNode> item, JsonGenerator jsonGenerator,
+                          SerializerProvider serializerProvider) throws IOException {
+            jsonGenerator.writeStartArray();
+//                jsonGenerator.writeArrayFieldStart("children");
+        for (var entry : item.entrySet()) {
+//            System.out.println(entry.getKey() + "/" + entry.getValue());
+            jsonGenerator.writeObject(entry.getValue());
+
+        }
+                jsonGenerator.writeEndArray();
+    }
+}
