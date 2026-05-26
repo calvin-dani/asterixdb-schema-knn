e36bfa06819e6ccbb2d630ca1e18bdbf20e7da66
Le0shy <leoshy1005@gmail.com>
2026-05-26 13:25:47 -0700

    [ASTERIXDB-3771] ANN query optimizer rule for top-k ANN vector search
    - user model changes: yes
    - storage format changes: no
    - interface changes: yes
    
    Add optimizer rules and physical operators for approximate nearest neighbor
    (ANN) top-k queries using VTree vector indexes.
    
    Components:
    - IntroduceTopKAccessMethodRule: matches LIMIT k -> ORDER BY ann_distance(...)
      pattern and transforms to vector index access
    - VectorIndexAccessMethod: implements IAccessMethod for vector index selection
    - VectorSearchPOperator: physical operator for vector search execution
    - PushFilterIntoVectorSearchRule: pushes INCLUDE field filters into vector search
    - ANNDistanceDescriptor: runtime evaluator for ann_distance function
    - VectorDistanceFunctionFactory: factory for distance metric implementations
    - Metadata/DDL integration for vector index query path
    
    Change-Id: Iaa502b57915c75921cd5aa5c62fc0e24261bb0f6

--- asterixdb/asterix-runtime/src/main/java/org/apache/asterix/runtime/functions/FunctionCollection.java
+++ asterixdb/asterix-runtime/src/main/java/org/apache/asterix/runtime/functions/FunctionCollection.java
@@ -647,6 +647,7 @@ import org.apache.asterix.runtime.evaluators.functions.temporal.WeekOfYear2Descr
 import org.apache.asterix.runtime.evaluators.functions.temporal.WeekOfYearDescriptor;
 import org.apache.asterix.runtime.evaluators.functions.temporal.YearMonthDurationGreaterThanComparatorDescriptor;
 import org.apache.asterix.runtime.evaluators.functions.temporal.YearMonthDurationLessThanComparatorDescriptor;
+import org.apache.asterix.runtime.evaluators.functions.vector.ANNDistanceDescriptor;
 import org.apache.asterix.runtime.evaluators.functions.vector.CosineDistanceDescriptor;
 import org.apache.asterix.runtime.evaluators.functions.vector.DotProductDescriptor;
 import org.apache.asterix.runtime.evaluators.functions.vector.EuclideanDistanceDescriptor;
@@ -1343,6 +1344,7 @@ public final class FunctionCollection implements IFunctionCollection {
         fc.add(EuclideanSquaredDistanceDescriptor.FACTORY);
         fc.add(CosineDistanceDescriptor.FACTORY);
         fc.add(DotProductDescriptor.FACTORY);
+        fc.add(ANNDistanceDescriptor.FACTORY);
 
         // Type functions.
         fc.add(GetTypeDescriptor.FACTORY);
